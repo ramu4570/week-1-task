df8d63bea4ef7388b11d31fdf37f02ca20e7e675
KUNDANAPALLY RAMU <ramukundanapally@gmail.com>
2026-07-03 12:54:59 +0530

    rectangle commit

---
+++ project1/src/project1/rectangle.java
@@ -0,0 +1,15 @@
+package project1;
+public class rectangle {
+	void calculatearea() {
+	int length=10;
+	int breadth=5;
+	int area=length*breadth;
+	System.out.println("area of rectangle:"+area);
+	}
+	public static void main(String[] args) {
+		rectangle re =new rectangle();
+		re.calculatearea();
+		
+	}
+
+}
